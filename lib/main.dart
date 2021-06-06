cbb2a437752e0c46e628f042281449f6ff0fb37e
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-06-06 11:06:31 +0430

    flutter_yellow_ui

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_yellow_ui/screen/Login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
